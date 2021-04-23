e7beeab3ecdbf842d50a9f37d067252359dd816b
aki <aki.akaguma@hotmail.com>
2021-04-24 08:10:39 +0900

    version 0.1.22

--- tests/test_s.rs
+++ tests/test_s.rs
@@ -19,6 +19,7 @@ macro_rules! help_msg {
             "\n",
             "  -H, --help        display this help and exit\n",
             "  -V, --version     display version information and exit\n",
+            "  -X <x-options>    x options. try -X help\n",
             "\n",
             "Option Parameters:\n",
             "  <exp>     regular expression, color the entire match. \n",
